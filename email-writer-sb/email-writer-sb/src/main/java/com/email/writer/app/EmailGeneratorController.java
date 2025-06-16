d081bf8a72b63e02008e3c150d066372ea48dfb9
RANGA KAVYA <124452221+kavya-ux-2@users.noreply.github.com>
2025-06-16 13:01:41 +0530

    Update EmailGeneratorController.java

--- email-writer-sb/email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorController.java
+++ email-writer-sb/email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/email")
 @AllArgsConstructor
-@CrossOrigin(origins = "https://email-ai-assistant-ruby.vercel.app/")
+@CrossOrigin(origins = "*")
 public class EmailGeneratorController {
 
     private final EmailGeneratorService emailGeneratorService;
